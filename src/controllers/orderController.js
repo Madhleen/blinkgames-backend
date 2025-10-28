c120f8eedbf2187b54f9a4eb82203324560552e0
Madhleen <madhleen@hotmail.com>
2025-10-27 23:48:09 -0300

    Corrige importação Mercado Pago (case-sensitive)

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -2,7 +2,8 @@ import Order from "../models/Order.js";
 import Raffle from "../models/Raffle.js";
 import User from "../models/User.js";
 import { gerarNumerosUnicos } from "../utils/numberGenerator.js";
-import { client, Preference } from "../config/mercadoPago.js";
+import { client, Preference } from "../config/mercadopago.js";
+
 
 
 // 🔹 Criar ordem e preference no Mercado Pago
